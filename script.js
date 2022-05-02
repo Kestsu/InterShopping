f21d346ccde40af01c84f9881f198a87662342c2
Kevin Li <Kevinstart14@gmail.com>
2022-05-02 15:46:29 -0300

    resolvido 2

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+event.target.remove();
+// console.log(event.target);
+localStorage.removeItem('produtos');
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function getSkuFromProductItem(item) {
+  // caminho pra pegar codigo  (filho do item que seja span e tenha essa class)
+  return item.querySelector('span.item__sku').innerText;
+}
+
+// Colocar do lado os items selecionados!! 
+async function appendCart(produto) {
+  // vai pegar as informacoes tudo 
+  const botao = produto.target;
+  // vai pegar o pai (section)) do botao e jogar na funcao
+  const codigo = getSkuFromProductItem(botao.parentElement);
+  const data = await fetchItem(codigo);
+ const { id, title, price } = data;
+  const salvar = document.querySelector('.cart__items')
+ .appendChild(createCartItemElement({ sku: id, name: title, salePrice: price }));
+ saveCartItems(salvar.innerHTML);
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -9,6 +42,7 @@ function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  if (element === 'button') e.addEventListener('click', appendCart);
   return e;
 }
 
@@ -24,6 +58,7 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+// Vai pegar cada produto e jogar na tela!! 
 async function appendProducts() {
   const products = await fetchProducts('computador');
   await products.forEach(({ id, title, thumbnail }) => {
@@ -33,55 +68,9 @@ async function appendProducts() {
       }));
   });
 }
-// const fetchProducts = (a) => {
-//   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${a}`)
-//   .then((primeiro) => primeiro.json())
-//   .then((segundo) => segundo.results)
-//   .catch((error) => error);
-// };
-
-// async function appendProducts() {
-//   await fetchProducts('computador')
-//    .then((response) => response.forEach(({ id, title, thumbnail }) => {
-//      document.querySelector('.items')
-//      .appendChild(createProductItemElement({
-//         sku: id, name: title, image: thumbnail,
-//        }));
-//    }))
-//    .catch((error) => error);
-//  }
-
-// Vai pegar cada produto e jogar na tela!! 
- 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-event.target.remove();
-// console.log(event.target);
-localStorage.removeItem('produtos');
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-// Colocar do lado os items selecionados!! 
-async function appendCart(produto) {
-  const data = await fetchItem(produto);
- const { id, title, price } = data;
-  const salvar = document.querySelector('.cart__items')
- .appendChild(createCartItemElement({ sku: id, name: title, salePrice: price }));
- saveCartItems(salvar.innerHTML);
-}
 
 window.onload = () => {
   appendProducts();
-  appendCart('MLB1341706310');
+  // appendCart('MLB1341706310');
 getSavedCartItems();
 };
