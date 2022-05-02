c269d6250bb2d218ec805f308d8b7df1d6ddce19
Kevin Li <Kevinstart14@gmail.com>
2022-05-02 16:56:50 -0300

    tentando 4

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const carrinho = document.querySelector('.cart__items');
+
 function cartItemClickListener(event) {
   // coloque seu código aqui
-event.target.remove();
-// console.log(event.target);
-localStorage.removeItem('produtos');
+  event.target.remove();
+  // console.log(event.target);
+  // localStorage.removeItem('produtos');
+  saveCartItems(carrinho.innerHTML);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -10,6 +13,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  // document.querySelectorAll('.cart__item').addEventListener('click', cartItemClickListener);
   return li;
 }
 
@@ -18,17 +22,20 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-// Colocar do lado os items selecionados!! 
+// Colocar do lado os items selecionados!!
 async function appendCart(produto) {
-  // vai pegar as informacoes tudo 
+  // vai pegar as informacoes tudo
   const botao = produto.target;
   // vai pegar o pai (section)) do botao e jogar na funcao
   const codigo = getSkuFromProductItem(botao.parentElement);
   const data = await fetchItem(codigo);
- const { id, title, price } = data;
-  const salvar = document.querySelector('.cart__items')
- .appendChild(createCartItemElement({ sku: id, name: title, salePrice: price }));
- saveCartItems(salvar.innerHTML);
+  const { id, title, price } = data;
+  document
+    .querySelector('.cart__items')
+    .appendChild(
+      createCartItemElement({ sku: id, name: title, salePrice: price }),
+    );
+    saveCartItems(carrinho.innerHTML);
 }
 
 function createProductImageElement(imageSource) {
@@ -53,24 +60,29 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
+  );
 
   return section;
 }
 
-// Vai pegar cada produto e jogar na tela!! 
+// Vai pegar cada produto e jogar na tela!!
 async function appendProducts() {
   const products = await fetchProducts('computador');
   await products.forEach(({ id, title, thumbnail }) => {
-    document.querySelector('.items')
-    .appendChild(createProductItemElement({
-       sku: id, name: title, image: thumbnail,
-      }));
+    document.querySelector('.items').appendChild(
+      createProductItemElement({
+        sku: id,
+        name: title,
+        image: thumbnail,
+      }),
+    );
   });
 }
 
 window.onload = () => {
   appendProducts();
   // appendCart('MLB1341706310');
-getSavedCartItems();
+  getSavedCartItems();
 };
